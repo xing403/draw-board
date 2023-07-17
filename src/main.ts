94b99665a8ecbea950938bfb47b0423f88c5d6fa
星如雨 <liyun200104@163.com>
2023-07-17 18:35:02 +0800

    chore: init

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+import ElementPlus from 'element-plus'
+import routes from 'virtual:generated-pages'
+import pinia from './store'
+import App from './App.vue'
+
+import '@unocss/reset/tailwind.css'
+import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import 'element-plus/theme-chalk/display.css'
+import 'uno.css'
+
+const app = createApp(App)
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+})
+app.use(pinia)
+app.use(ElementPlus)
+app.use(router)
+app.mount('#app')
