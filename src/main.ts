563cf34011d1ab5a65bffde25c72e0707ce5ac80
星如雨 <liyun200104@163.com>
2023-07-22 22:41:14 +0800

    test build app

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import ElementPlus from 'element-plus'
 import routes from 'virtual:generated-pages'
@@ -14,7 +14,7 @@ import 'uno.css'
 
 const app = createApp(App)
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 })
 app.use(pinia)
