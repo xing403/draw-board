94c29ecc098e2ca9ac26223487fb49713673c27c
星如雨 <liyun200104@163.com>
2023-07-28 11:03:58 +0800

    update information

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 
 import ElementPlus from 'element-plus'
 import routes from 'virtual:generated-pages'
+import packageJson from '../package.json'
 import pinia from './store'
 import App from './App.vue'
 
@@ -22,3 +23,9 @@ app.use(pinia)
 app.use(ElementPlus)
 app.use(router)
 app.mount('#app')
+
+// eslint-disable-next-line no-console
+console.log(`%c version %c ${packageJson.version} `,
+  'color:#FFF;background:#5c5c5c;border-radius:5px 0 0 5px;padding:5px;margin: 5px 0',
+  'color:#FFF;background:#E6A23C;border-radius:0 5px 5px 0;padding:5px;margin: 5px 0',
+)
